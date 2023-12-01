4b72d9f68f0eafe8e8e1ad7685bd7a597f84d841
Your Name <you@example.com>
2023-12-01 15:39:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
 using namespace std;
 
+
+int x,odd = 0,even = 0;
+
+
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> x;
+
+        if(x == 0){
+
+            break;
+
+        }else if(x%2 == 0){
+
+            even++;
+        }else if(x%2 != 0){
+
+            odd++;
+        }
+       
+
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
